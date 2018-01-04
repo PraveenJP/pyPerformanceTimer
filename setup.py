679c6f6600067fa6fcef03b1027d170b533718a6
praveenjp <roadizpraveen@gmail.com>
2018-01-04 08:53:35 +0530

    Package setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_performance_timer',
+      version='0.1',
+      description='Used to find the function execution time',
+      url='https://github.com/PraveenJP/pyPerformanceTimer',
+      author='Praveen J',
+      author_email='praveen.josephmasilamani@outlook.com',
+      license='MIT',
+      packages=['pref_timer'],
+      zip_safe=False)
